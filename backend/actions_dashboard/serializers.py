2cf624f6b52d400ac5684ea442fa3e8d31bc24f8
Andrew Scherer <ascherer97@gmail.com>
2025-07-07 16:05:46 -0700

    implement public facing api

--- backend/actions_dashboard/serializers.py
+++ backend/actions_dashboard/serializers.py
@@ -1,3 +1,54 @@
 import pprint
 
 from rest_framework import serializers
+
+from webhook.models import GitHubRepository, GitHubUser, GitHubWorkflow, GitHubWorkflowRun
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["id"] = data.pop("github_id", None)
+        data["login"] = data.pop("username", None)
+        return data
+
+    class Meta:
+        model = GitHubUser
+        fields = ["github_id", "node_id", "username", "html_url"]
+        read_only_fields = ["github_id", "node_id", "username", "html_url"]
+
+
+class RepositorySerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["id"] = data.pop("github_id", None)
+        return data
+
+    class Meta:
+        model = GitHubRepository
+        fields = ["github_id", "node_id", "html_url", "stargazers_count", "forks_count", "owner"]
+        read_only_fields = ["github_id", "node_id", "html_url", "stargazers_count", "forks_count", "owner"]
+
+
+class WorkflowSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["id"] = data.pop("github_id", None)
+        return data
+
+    class Meta:
+        model = GitHubWorkflow
+        fields = ["github_id", "node_id", "html_url", "repository"]
+        read_only_fields = ["github_id", "node_id", "html_url", "repository"]
+
+
+class WorkflowRunSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["updated_at"] = data.pop("github_updated_at", None)
+        return data
+
+    class Meta:
+        model = GitHubWorkflowRun
+        fields = ["action", "workflow", "conclusion", "name", "url", "display_title", "github_updated_at"]
+        read_only_fields = ["action", "workflow", "conclusion", "name", "url", "display_title", "github_updated_at"]
--- backend/actions_dashboard/views.py
+++ backend/actions_dashboard/views.py
@@ -9,38 +9,57 @@ from webhook.models import (
     GitHubWorkflow,
     GitHubWorkflowRun,
 )
-from webhook.serializers import (
-    GitHubRepositorySerializer,
-    GitHubUserSerializer,
+
+from .serializers import (
+    RepositorySerializer,
+    UserSerializer,
+    WorkflowRunSerializer,
+    WorkflowSerializer,
 )
 
 
-class GitHubUserViewSet(viewsets.ModelViewSet):
+class GitHubUserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = GitHubUser.objects.all()
-    serializer_class = GitHubUserSerializer
+    serializer_class = UserSerializer
 
-    @action(detail=False, methods=["get"])
-    def search(self, request):
-        """
-        Search for GitHub users by username or name.
-        """
-        query = request.query_params.get("q", "")
-        if not query:
-            return Response({"error": "Query parameter 'q' is required"}, status=status.HTTP_400_BAD_REQUEST)
+    # def search(self, request):
+    #     """
+    #     Search for GitHub users by username or name.
+    #     """
+    #     query = request.query_params.get("q", "")
+    #     if not query:
+    #         return Response({"error": "Query parameter 'q' is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Filter users based on the query
-        users = GitHubUser.objects.filter(Q(username__icontains=query) | Q(name__icontains=query)).distinct()
+    #     # Filter users based on the query
+    #     users = GitHubUser.objects.filter(Q(username__icontains=query) | Q(name__icontains=query)).distinct()
 
-        serializer = GitHubUserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    #     serializer = UserSerializer(users, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def retrieve(self, request, pk=None):
-        """
-        Retrieve a specific GitHub user by ID.
-        """
-        try:
-            user = GitHubUser.objects.get(pk=pk)
-            serializer = GitHubUserSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except GitHubUser.DoesNotExist:
-            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+    # @action(detail=False, methods=["get"])
+    # def retrieve(self, request, pk=None):
+    #     """
+    #     Retrieve a specific GitHub user by ID.
+    #     """
+    #     try:
+    #         user = GitHubUser.objects.get(pk=pk)
+    #         serializer = UserSerializer(user)
+    #         return Response(serializer.data, status=status.HTTP_200_OK)
+    #     except GitHubUser.DoesNotExist:
+    #         return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class GitHubRepositoryViewSet(viewsets.ModelViewSet):
+    queryset = GitHubRepository.objects.all()
+    serializer_class = RepositorySerializer
+
+
+class GitHubWorkflowViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = GitHubWorkflow.objects.all()
+    serializer_class = WorkflowSerializer
+
+
+
+class GitHubWorkflowRunViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = GitHubWorkflowRun.objects.all()
+    serializer_class = WorkflowRunSerializer
