faff24f76152dd046d8c3d02b726cdc0fd650cf2
Andrew Scherer <ascherer97@gmail.com>
2025-07-11 19:33:52 -0700

    swap navbar to use media queries instead of state

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import DarkModeSwitch from "./DarkModeSwitch";
 import GitHub from "./ui/GitHubMark";
-import { Link } from "@tanstack/react-router";
-import { useWidth } from "~/hooks/useWindowSize";
+import { Link, useLocation } from "@tanstack/react-router";
 import { Popover } from "radix-ui";
 import { MenuIcon, HomeIcon } from "lucide-react";
 import { Route as ProjectsRoute } from "~/routes/projects.index";
 import { Route as BlogRoute } from "~/routes/blog.index";
 import { Route as AboutRoute } from "~/routes/about";
 import { cn } from "~/utils/utils";
+import { useEffect, useState } from "react";
 
 export interface NavbarProps {
   height: string;
@@ -37,16 +37,72 @@ const navStyles = {
 };
 
 export default function Navbar() {
-  const { width } = useWidth();
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = useLocation().pathname;
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   return (
     <div className={navStyles.container}>
-      {width >= 768 ? (
-        <nav
-          className={cn(
-            navStyles.nav,
-            "justify-between grid px-5 grid-cols-3 max-w-6xl"
-          )}
+      <nav
+        className={cn(
+          navStyles.nav,
+          "justify-between px-5 grid-cols-3 max-w-6xl hidden md:grid"
+        )}
+      >
+        <Link
+          to="/"
+          activeProps={{ className: navStyles.active }}
+          inactiveProps={{ className: navStyles.inactive }}
+          activeOptions={{ exact: true }}
+          className="flex cursor-pointer items-center justify-center px-1 py-0 m-0 size-[3rem] rounded-full bg-transparent shadow-none border-solid focus-visible:ring-1 focus-visible:ring-blue-500 hover:bg-gray-600/20"
+          resetScroll
+          aria-label="Home page link"
         >
+          <HomeIcon className="p-0" size={32} aria-label="Home icon" />
+        </Link>
+        <div className="align-middle grow-0 flex px-5">
+          <Link
+            to={BlogRoute.to}
+            activeProps={{ className: navStyles.active }}
+            inactiveProps={{ className: navStyles.inactive }}
+            className={navStyles.button}
+            resetScroll
+          >
+            Blog
+          </Link>
+          <Link
+            to={ProjectsRoute.to}
+            activeProps={{ className: navStyles.active }}
+            inactiveProps={{ className: navStyles.inactive }}
+            className={navStyles.button}
+            resetScroll
+          >
+            Projects
+          </Link>
+          <Link
+            to={AboutRoute.to}
+            activeProps={{ className: navStyles.active }}
+            inactiveProps={{ className: navStyles.inactive }}
+            className={navStyles.button}
+            resetScroll
+          >
+            About
+          </Link>
+        </div>
+        <div className="flex items-center justify-end grow-0">
+          <div className="px-2">
+            <GitHub githubLink={"https://github.com/andrew-s28/"} />
+          </div>
+          <div className="px-2">
+            <DarkModeSwitch />
+          </div>
+        </div>
+      </nav>
+      <nav className={cn(navStyles.nav, "flex justify-center md:hidden")}>
+        <div className="flex items-center justify-between w-full px-2">
           <Link
             to="/"
             activeProps={{ className: navStyles.active }}
@@ -54,125 +110,72 @@ export default function Navbar() {
             activeOptions={{ exact: true }}
             className="flex cursor-pointer items-center justify-center px-1 py-0 m-0 size-[3rem] rounded-full bg-transparent shadow-none border-solid focus-visible:ring-1 focus-visible:ring-blue-500 hover:bg-gray-600/20"
             resetScroll
-            aria-label="Home page link"
           >
-            <HomeIcon className="p-0" size={32} aria-label="Home icon" />
+            <HomeIcon className="p-0" size={32} />
           </Link>
-          <div className="align-middle grow-0 flex px-5">
-            <Link
-              to={BlogRoute.to}
-              activeProps={{ className: navStyles.active }}
-              inactiveProps={{ className: navStyles.inactive }}
-              className={navStyles.button}
-              resetScroll
-            >
-              Blog
-            </Link>
-            <Link
-              to={ProjectsRoute.to}
-              activeProps={{ className: navStyles.active }}
-              inactiveProps={{ className: navStyles.inactive }}
-              className={navStyles.button}
-              resetScroll
-            >
-              Projects
-            </Link>
-            <Link
-              to={AboutRoute.to}
-              activeProps={{ className: navStyles.active }}
-              inactiveProps={{ className: navStyles.inactive }}
-              className={navStyles.button}
-              resetScroll
-            >
-              About
-            </Link>
-          </div>
-          <div className="flex items-center justify-end grow-0">
-            <div className="px-2">
-              <GitHub githubLink={"https://github.com/andrew-s28/"} />
-            </div>
-            <div className="px-2">
-              <DarkModeSwitch />
-            </div>
-          </div>
-        </nav>
-      ) : (
-        <nav className={cn(navStyles.nav, "flex justify-center")}>
-          <div className="flex items-center justify-between w-full px-2">
-            <Link
-              to="/"
-              activeProps={{ className: navStyles.active }}
-              inactiveProps={{ className: navStyles.inactive }}
-              activeOptions={{ exact: true }}
-              className="flex cursor-pointer items-center justify-center px-1 py-0 m-0 size-[3rem] rounded-full bg-transparent shadow-none border-solid focus-visible:ring-1 focus-visible:ring-blue-500 hover:bg-gray-600/20"
-              resetScroll
-            >
-              <HomeIcon className="p-0" size={32} />
-            </Link>
 
-            <div className="align-middle justify-center grow-0 px-4">
-              <Popover.Root>
-                <Popover.Trigger className="flex items-center justify-center h-full cursor-pointer outline-0 focus-visible:ring-1 focus-visible:ring-blue-500">
-                  <div className="flex items-center justify-center h-full cursor-pointer outline-0 focus-visible:ring-1 focus-visible:ring-blue-500">
-                    <MenuIcon className="w-full" size={32} />
-                    <span className="sr-only">Toggle menu</span>
-                  </div>
-                </Popover.Trigger>
-                <Popover.Portal>
-                  <Popover.Content
-                    className="w-64 bg-dawn-pink-100 dark:bg-night-sky-950 p-2 rounded-lg shadow-xl z-50 border-night-sky-950 dark:border-dawn-pink-100 border-2"
-                    sideOffset={5}
-                  >
-                    <div className="flex flex-col space-y-1">
-                      <Link
-                        to={BlogRoute.to}
-                        className="px-4 py-2 hover:bg-muted rounded-md text-center"
-                        activeProps={{
-                          className:
-                            "font-bold bg-dawn-pink-300 dark:bg-night-sky-900",
-                        }}
-                        resetScroll
-                      >
-                        Blog
-                      </Link>
-                      <Link
-                        to={ProjectsRoute.to}
-                        className="px-4 py-2 hover:bg-muted rounded-md text-center"
-                        activeProps={{
-                          className:
-                            "font-bold bg-dawn-pink-300 dark:bg-night-sky-900",
-                        }}
-                        resetScroll
-                      >
-                        Projects
-                      </Link>
-                      <Link
-                        to={AboutRoute.to}
-                        className="px-4 py-2 hover:bg-muted rounded-md text-center"
-                        activeProps={{
-                          className:
-                            "font-bold bg-dawn-pink-300 dark:bg-night-sky-900",
-                        }}
-                        resetScroll
-                      >
-                        About
-                      </Link>
-                      <div className="px-2 flex items-center justify-center">
-                        <GitHub githubLink={"https://github.com/andrew-s28/"} />
-                        <DarkModeSwitch />
-                      </div>
+          <div className="align-middle justify-center grow-0 px-4">
+            <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
+              <Popover.Trigger className="flex items-center justify-center h-full cursor-pointer outline-0 focus-visible:ring-1 focus-visible:ring-blue-500">
+                <div className="flex items-center justify-center h-full cursor-pointer outline-0 focus-visible:ring-1 focus-visible:ring-blue-500">
+                  <MenuIcon className="w-full" size={32} />
+                  <span className="sr-only">Toggle menu</span>
+                </div>
+              </Popover.Trigger>
+              <Popover.Portal>
+                <Popover.Content
+                  className="w-64 bg-dawn-pink-100 dark:bg-night-sky-950 p-2 rounded-lg shadow-xl z-50 border-night-sky-950 dark:border-dawn-pink-100 border-2"
+                  sideOffset={5}
+                >
+                  <div className="flex flex-col space-y-1">
+                    <Link
+                      to={BlogRoute.to}
+                      className="px-4 py-2 hover:bg-muted rounded-md text-center"
+                      activeProps={{
+                        className:
+                          "font-bold bg-dawn-pink-300 dark:bg-night-sky-900",
+                      }}
+                      resetScroll
+                    >
+                      Blog
+                    </Link>
+                    <Link
+                      to={ProjectsRoute.to}
+                      className="px-4 py-2 hover:bg-muted rounded-md text-center"
+                      activeProps={{
+                        className:
+                          "font-bold bg-dawn-pink-300 dark:bg-night-sky-900",
+                      }}
+                      resetScroll
+                    >
+                      Projects
+                    </Link>
+                    <Link
+                      to={AboutRoute.to}
+                      className="px-4 py-2 hover:bg-muted rounded-md text-center"
+                      activeProps={{
+                        className:
+                          "font-bold bg-dawn-pink-300 dark:bg-night-sky-900",
+                      }}
+                      resetScroll
+                    >
+                      About
+                    </Link>
+                    <div className="px-2 flex items-center justify-center">
+                      <GitHub githubLink={"https://github.com/andrew-s28/"} />
+                      <DarkModeSwitch />
                     </div>
-                    <Popover.Arrow
-                      height={10}
-                      className="dark:fill-dawn-pink-100 fill-night-sky-950"
-                    />
-                  </Popover.Content>
-                </Popover.Portal>
-              </Popover.Root>
-            </div>
+                  </div>
+                  <Popover.Arrow
+                    height={10}
+                    className="dark:fill-dawn-pink-100 fill-night-sky-950"
+                  />
+                </Popover.Content>
+              </Popover.Portal>
+            </Popover.Root>
           </div>
-        </nav>
-      )}
+        </div>
+      </nav>
     </div>
   );
 }
